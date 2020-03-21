996e94183705f27778d3788be76335819cb5615e
Adam Beck <akb93@case.edu>
2020-03-21 16:42:25 -0400

    Solve buy_and_sell_stock_twice

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -4,8 +4,29 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    def calc_best_forwards():
+        best_forwards = [0] * len(prices)
+        best = 0
+        min_stock = prices[0]
+
+        for i, x in enumerate(prices):
+            min_stock = min(min_stock, x)
+            best = max(best, x - min_stock)
+            best_forwards[i] = best
+
+        return best_forwards
+
+    best_forwards = calc_best_forwards()
+    best_profit = best_forwards[-1]
+
+    # go in reverse
+    max_stock = prices[-1]
+    for i in range(len(prices)-1, 0, -1): # intentionally don't look at the 0-th element
+        x = prices[i]
+        max_stock = max(max_stock, x)
+        best_profit = max(best_profit, max_stock - x + best_forwards[i-1])
+
+    return best_profit
 
 
 if __name__ == '__main__':
