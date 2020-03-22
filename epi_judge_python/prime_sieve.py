6ddfac5385b37ff3494d3b20a7adcd4e1e98b1bb
Adam Beck <akb93@case.edu>
2020-03-21 20:03:38 -0400

    Improve prime_sieve

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -6,42 +6,21 @@ from test_framework import generic_test
 
 
 class PrimeStatus(Enum):
-    UNKNOWN = auto()
     PRIME = auto()
     NOT_PRIME = auto()
-
-    @classmethod
-    def is_prime(cls, n):
-        if n == 2:
-            return cls.PRIME
-        if n < 2 or n % 2 == 0:
-            return cls.NOT_PRIME
-
-        i = 3
-        while i*i <= n:
-            if n % i == 0:
-                return cls.NOT_PRIME
-            i += 2
-
-        return cls.PRIME
 
 
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    are_primes = [PrimeStatus.UNKNOWN] * (n+1)
-
+    are_primes = [PrimeStatus.NOT_PRIME, PrimeStatus.NOT_PRIME] + [PrimeStatus.PRIME] * n
     primes = []
 
     for i in range(2, n+1):
-        if are_primes[i] == PrimeStatus.UNKNOWN:
-            are_primes[i] = PrimeStatus.is_prime(i)
-
-            if are_primes[i] == PrimeStatus.NOT_PRIME:
-                for k in range(i, n, i):
-                    are_primes[k] = PrimeStatus.NOT_PRIME
-
         if are_primes[i] == PrimeStatus.PRIME:
             primes.append(i)
+
+            for k in range(i, n+1, i):
+                are_primes[k] = PrimeStatus.NOT_PRIME
 
     return primes
 
