5ef809f3fa3d638db3529ee0291198c62e34b52b
Adam Beck <akb93@case.edu>
2020-03-22 22:04:36 -0400

    Solve spiral_ordering

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -1,11 +1,34 @@
+from enum import Enum, auto
 from typing import List
 
 from test_framework import generic_test
 
 
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    def traverse_row(row_index, start, end):
+        inc = 1 if start < end else -1
+        return [square_matrix[row_index][i] for i in range(start, end, inc)]
+
+    def traverse_col(col_index, start, end):
+        inc = 1 if start < end else -1
+        return [square_matrix[i][col_index] for i in range(start, end, inc)]
+
+    ans = []
+    l, r = 0, len(square_matrix)-1
+
+    while l < r:
+        ans.extend(traverse_row(l, l, r))
+        ans.extend(traverse_col(r, l, r))
+        ans.extend(traverse_row(r, r, l))
+        ans.extend(traverse_col(l, r, l))
+
+        l += 1
+        r -= 1
+
+    if l == r:
+        ans.append(square_matrix[l][r])
+
+    return ans
 
 
 if __name__ == '__main__':
