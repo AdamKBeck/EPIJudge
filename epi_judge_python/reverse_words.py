7591034a775bd0ee92151b5665310753e6c7a82c
Adam Beck <akb93@case.edu>
2020-03-26 20:04:15 -0400

    Solve reverse_words

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -2,13 +2,31 @@ import functools
 
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
+from typing import List
 
 
 # Assume s is a list of strings, each of which is of length 1, e.g.,
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
-def reverse_words(s):
-    # TODO - you fill in here.
-    return
+def reverse_words(s: List[str]):
+    def flip(i, j):
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+
+    s.reverse()
+    i, j = 0, 0
+
+    while i < len(s):
+        while j < len(s) and s[j] != ' ':
+            j += 1
+
+        flip(i, j-1)
+        j += 1
+        i = j
+
+    return s
+
 
 
 @enable_executor_hook
