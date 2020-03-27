6ea1dc45feb793df42122eda98deb11395988bf7
Adam Beck <akb93@case.edu>
2020-03-27 19:55:57 -0400

    Solve look_and_say

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -1,9 +1,18 @@
+from itertools import groupby
 from test_framework import generic_test
 
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    s = '1'
+
+    for _ in range(1, n):
+        ans = ""
+        for val, group in groupby(s):
+            count = str(len(list(group)))
+            ans += count + val
+        s = ans
+
+    return s
 
 
 if __name__ == '__main__':
