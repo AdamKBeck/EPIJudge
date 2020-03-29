f77e43fa92dfe41c0341b3df49e1632015798b43
Adam Beck <akb93@case.edu>
2020-03-29 18:26:42 -0400

    Solve roman_to_integer.py

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -1,9 +1,27 @@
 from test_framework import generic_test
 
 
+ROMAN_TO_INT = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+}
+
+
 def roman_to_integer(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    acc = 0
+
+    for i, x in enumerate(s):
+        if i+1 < len(s) and ROMAN_TO_INT[x] < ROMAN_TO_INT[s[i+1]]:
+            acc += -ROMAN_TO_INT[x]
+        else:
+            acc += ROMAN_TO_INT[x]
+
+    return acc
 
 
 if __name__ == '__main__':
