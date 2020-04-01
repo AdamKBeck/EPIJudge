f2575ab0d6658d457665825feb85a49033addf1c
Adam Beck <akb93@case.edu>
2020-03-31 20:39:42 -0400

    Solve sorted_lists_merge

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -6,8 +6,21 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    head = ListNode()
+    cpy = head
+
+    while L1 and L2:
+        if L1.data < L2.data:
+            head.next = L1
+            L1 = L1.next
+        else:
+            head.next = L2
+            L2 = L2.next
+
+        head = head.next
+
+    head.next = L1 or L2
+    return cpy.next
 
 
 if __name__ == '__main__':
