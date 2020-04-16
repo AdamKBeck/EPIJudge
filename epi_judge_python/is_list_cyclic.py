0c8ed0e422d07aeee4717c73e875b19aee66d6b2
Adam Beck <akb93@case.edu>
2020-04-16 00:54:24 -0400

    Solve is_list_cyclic

--- epi_judge_python/is_list_cyclic.py
+++ epi_judge_python/is_list_cyclic.py
@@ -8,7 +8,16 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def has_cycle(head: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
+    slow = head
+    fast = head
+
+    while slow and fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow is fast:
+            return slow
+
     return None
 
 
