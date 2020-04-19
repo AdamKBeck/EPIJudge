a0c1c4570ab4eeb5df0788f1a74ee9286315a9a1
Adam Beck <akb93@case.edu>
2020-04-19 16:04:12 -0400

    Solve list_cyclic_right_shift

--- epi_judge_python/list_cyclic_right_shift.py
+++ epi_judge_python/list_cyclic_right_shift.py
@@ -5,8 +5,30 @@ from test_framework import generic_test
 
 
 def cyclically_right_shift_list(L: ListNode, k: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    def tail(x):
+        acc = 0
+        last_seen = None
+        while x:
+            acc += 1
+            last_seen = x
+            x = x.next
+        return acc, last_seen
+
+    if not L or not L.next:
+        return L
+
+    n, end = tail(L)
+    end.next = L  # connect the end with the start
+    k %= n  # k can be much bigger than n, so we can reduce the unneeded iteration
+
+    # iterate to our new end
+    curr = L
+    for _ in range(n-k-1):
+        curr = curr.next
+
+    new_head = curr.next
+    curr.next = None
+    return new_head
 
 
 if __name__ == '__main__':
