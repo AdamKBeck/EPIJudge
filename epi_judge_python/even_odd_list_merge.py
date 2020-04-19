c7e3ceeb4f20789870db72c7af3a85dae6797131
Adam Beck <akb93@case.edu>
2020-04-19 02:52:59 -0400

    Solve even_odd_list_merge

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -5,12 +5,29 @@ from test_framework import generic_test
 
 
 def even_odd_merge(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    odd_head, even_head = ListNode(), ListNode()
+    odd_last, even_last = odd_head, even_head
+
+    is_even = True
+    while L:
+        if is_even:
+            even_last.next = L
+            even_last = L
+        else:
+            odd_last.next = L
+            odd_last = L
+
+        L = L.next
+        is_even = not is_even
+
+    even_last.next = odd_head.next
+    odd_last.next = None
+    return even_head.next
 
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('even_odd_list_merge.py',
                                        'even_odd_list_merge.tsv',
-                                       even_odd_merge))
+                                       even_odd_merge)
+    )
