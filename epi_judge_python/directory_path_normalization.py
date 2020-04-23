fe23008f265ff9e275eb3110d2be2a106715002f
Adam Beck <akb93@case.edu>
2020-04-23 19:43:54 -0400

    Solve directory_path_normalization

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    stack = []
+
+    for x in path.split('/'):
+        if not x or x == '.':
+            continue
+        elif x == '..':
+            if stack and stack[-1] != '..':
+                stack.pop()
+            else:
+                stack.append(x)
+        else:
+            stack.append(x)
+
+    optional_start = '/' if path[0] == '/' else ''
+    return optional_start + '/'.join(stack)
 
 
 if __name__ == '__main__':
