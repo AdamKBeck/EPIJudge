838a3884d412ba94d85788035806944b9cc41512
Adam Beck <akb93@case.edu>
2020-04-28 23:11:47 -0400

    Solve tree_level_order

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -7,7 +7,7 @@ from test_framework import generic_test
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
     if not tree:
-        return None
+        return []
 
     ans = []
     current_level = [tree]
